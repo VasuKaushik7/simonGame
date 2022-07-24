ec9edf0a316ca73c3cf39eea2099ac9a7ff7a472
Vasu Kaushik <vasu.kaushik@tcs.com>
2022-07-25 04:50:38 +0530

    fix

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ var isGameStarted = false;
 var globalGameArray = [];
 var stage = 1;
 var correctlyidentified = 0;
-var buttonClicked=false;
-var gameOver=false;
+var buttonClicked = false;
+var gameOver = false;
 
 $(document).click(function () {
-    if(!isGameStarted){ // game starts
-    gameStart();
+    if (!isGameStarted) { // game starts
+        gameStart();
     }
-    if(gameOver){ // if game is over then on click start game again
+    if (gameOver) { // if game is over then on click start game again
         $('body').css('background-color', '#011F3F');
-        isGameStarted=false;
+        isGameStarted = false;
         console.log("startting new game");
-        stage=1;
-        correctlyidentified=0;
-        globalGameArray=[];
-        gameOver=false;
+        stage = 1;
+        correctlyidentified = 0;
+        globalGameArray = [];
+        gameOver = false;
         gameStart();
 
     }
@@ -26,22 +26,35 @@ $(document).click(function () {
 
 function gameStart() {
 
-    if(!isGameStarted || buttonClicked){ // only generate random button when game starts and when next stage is called.
-    isGameStarted = true;
+    if (!isGameStarted) { // only generate random button when game starts and when next stage is called.
+        isGameStarted = true;
+        $('#level-title').text('Stage ' + stage);
+        var randomButton = Math.floor(Math.random() * 4);
+        $('.' + globarStarterArray[randomButton]).fadeOut().fadeIn();
+        var audio = new Audio('sounds/' + globarStarterArray[randomButton] + '.mp3');
+        audio.play();
+        globalGameArray.push(globarStarterArray[randomButton]);
+    }
+    if (buttonClicked) {
+        setTimeout(() => {
+            delayedAnimation()
+        }, 300);
+    }
+}
+function delayedAnimation() {
     $('#level-title').text('Stage ' + stage);
     var randomButton = Math.floor(Math.random() * 4);
     $('.' + globarStarterArray[randomButton]).fadeOut().fadeIn();
     var audio = new Audio('sounds/' + globarStarterArray[randomButton] + '.mp3');
     audio.play();
     globalGameArray.push(globarStarterArray[randomButton]);
-    buttonClicked=false;
-    }
+    buttonClicked = false;
 }
 
 $('.btn').click(function (event) {
     if (isGameStarted && !gameOver)  //only do the animation when the game starts
-    { 
-         $('.' + event.currentTarget.id).addClass('pressed');
+    {
+        $('.' + event.currentTarget.id).addClass('pressed');
         setTimeout(() => {
             $('.' + event.currentTarget.id).removeClass('pressed');
         }, 100);
@@ -54,13 +67,13 @@ $('.btn').click(function (event) {
                 // pattern matched fully go to next stage
                 stage++
                 $('#level-title').text('Stage ' + stage);
-                correctlyidentified=0;
-                buttonClicked=true;
+                correctlyidentified = 0;
+                buttonClicked = true;
                 gameStart();
-            } 
+            }
             else {
                 // Wait for users next click
-                
+
             }
 
         }
@@ -68,15 +81,15 @@ $('.btn').click(function (event) {
             console.log("ending game");
             $('#level-title').text('Game Over Touch anywhere to Restart');
             $('body').css('background-color', 'red');
-            correctlyidentified=0;
+            correctlyidentified = 0;
             var audio = new Audio('sounds/wrong.mp3');
             audio.play();
             setTimeout(() => {
-                gameOver=true;
+                gameOver = true;
             }, 1000);
         }
     }
-    else{
+    else {
         // Game already over
     }
 })
